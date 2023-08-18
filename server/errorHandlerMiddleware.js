ea7eb72b260868f54d763ed3fd8af26fadaf10dd
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-08-18 14:30:07 +1200

    Added syntax for error-handling middleware

--- server/errorHandlerMiddleware.js
+++ server/errorHandlerMiddleware.js
@@ -1,5 +1,6 @@
 const errorHandlerMiddleware = (err, req, res, next) => {
   // TODO: Implement the error-handling middleware function
+  res.status(500).send('Internal Server Error!') 
 };
 
 module.exports = errorHandlerMiddleware;
